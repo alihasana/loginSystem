10a8b435a7bacb19d5b3bca54b143da8a84d45bd
Yoann Roulé <yoann.roule@gmail.com>
2018-03-01 15:49:06 +0100

    user model first paint

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import auth from './auth/route';
+import express from 'express'
+import mongoose from 'mongoose'
+import auth from './auth/route'
 
 let app = express();
-mongoose.connect('mongodb://localhost/loginsystem);
+mongoose.connect('mongodb://localhost:27017/loginsystem');
 
 app.use('/auth', auth);
 
